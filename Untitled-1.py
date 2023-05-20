660de290776874466e3a87503e0d79be0b517d79
TRYTEHb <133570604+TRYTEHb@users.noreply.github.com>
2023-05-20 12:31:20 +0300

    Add files via upload
    Added movement of objects, as well as collision.

--- Untitled-1.py
+++ Untitled-1.py
@@ -12,7 +12,46 @@ pygame.display.set_caption('Ping-pong')
 
 ball = pygame.Rect(win_width/2 -15, win_height/2 - 15,30,30)
 player1 = pygame.Rect(win_width-20, win_height/2 - 70,10,140)
-player2 = pygame.Rect(20, win_height/2 - 70,10,140)
+opponent = pygame.Rect(20, win_height/2 - 70,10,140)
+
+def player_animation():
+    player1.y += player_speed
+    if player1.top <= 0:
+        player1.top = 0
+    if player1.bottom >= win_height:
+        player1.bottom = win_height
+
+def opponent_ai():
+	if opponent.top < ball.y:
+		opponent.y += opponent_speed
+	if opponent.bottom > ball.y:
+		opponent.y -= opponent_speed
+
+	if opponent.top <= 0:
+		opponent.top = 0
+	if opponent.bottom >= win_height:
+		opponent.bottom = win_height
+
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= win_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= win_width:
+        ball_speed_x *= -1
+
+    if ball.colliderect(player1) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+ball_speed_x = 7
+ball_speed_y = 7
+player_speed = 0
+opponent_speed = 7
+
+score1 = 0
+score2 = 0
 
 bg_color = pygame.Color('grey12')
 light_gray = (200,200,200)
@@ -25,8 +64,27 @@ while run:
         if e.type == pygame.QUIT:
             run = False
 
+        if e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_DOWN:
+                player_speed += 7
+            if e.key == pygame.K_UP:
+                player_speed -= 7
+        if e.type == pygame.KEYUP:
+            if e.key == pygame.K_DOWN:
+                player_speed -= 7
+            if e.key == pygame.K_UP:
+                player_speed += 7
+
+    ball_animation()
+    player_animation()
+    opponent_ai()
+
+    window.fill(bg_color)
     pygame.draw.rect(window,red,player1)
-    pygame.draw.rect(window,blue,player2)
-    pygame.draw.rect(window,light_gray,ball)
+    pygame.draw.rect(window,blue,opponent)
+    pygame.draw.ellipse(window,light_gray,ball)
+    pygame.draw.aaline(window,light_gray,(win_width/2,0),(win_width/2,win_height))
+
+    
     pygame.display.update()
     clock.tick(60)
